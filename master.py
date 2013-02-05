8603a2269e163a032878a91c2b94583cbf3410c8
Robert Evans <rlevans91@gmail.com>
2013-02-05 21:59:27 +0000

    Improved multidimensional dtw comparisons (I hope)

--- master.py
+++ master.py
@@ -7,33 +7,34 @@ import quats
 import segment
 import pylab as pl
 
-def compareTwoFiles(f1,f2):
+def compareTwoFiles(f1,f2,n_components=3):
 	data1 = readCSVfile(f1)
 	data2 = readCSVfile(f2)
-	ls1 = getLowDimensionalSegments(data1)[1:-1]
-	ls2 = getLowDimensionalSegments(data2)[1:-1]
-	
+	(ls1,w1) = getLowDimensionalSegments(data1,n_components)
+	(ls2,w2) = getLowDimensionalSegments(data2,n_components)
+	averageWeights = [(a+b)/2.0 for (a,b) in zip(w1,w2)]
+	print averageWeights
 	names = []
 	for i in range(len(ls1)):
 		names.append('1')
 	for i in range(len(ls2)):
 		names.append('2')
-		
-	similarityMatrix(ls1+ls2,names)
 
-def similarityMatrix(segments,segmentNames):
+	similarityMatrix(ls1+ls2,names,averageWeights)
+
+def similarityMatrix(segments,segmentNames,weights):
 	distances = []
 	for i in range(len(segments)):
 		distances.append([])
 		for j in range(len(segments)):
-			distances[i].append(dtw.getDTWdist2D(segments[i],segments[j]))
+			distances[i].append(dtw.getDTWdist2DweightedSum(segments[i],segments[j],weights))
 	plot.plotSimilarityMatrix(distances,segmentNames)
 
-def getLowDimensionalSegments(highDimensionalData):
-	(lowDimensionalData,explainedVariance) = pca.pca(highDimensionalData,n_components=2)
+def getLowDimensionalSegments(highDimensionalData,n_components=2):
+	(lowDimensionalData,explainedVariance) = pca.pca(highDimensionalData,n_components)
 	(mins,maxs) = segment.segmentationPoints(lowDimensionalData[:,0])
 	segments = pl.split(lowDimensionalData,maxs)
-	return segments
+	return (segments[1:-1],explainedVariance)
 
 def getHighDimensionalSegments(highDimensionalData):
 	(lowDimensionalData,explainedVariance) = pca.pca(highDimensionalData,n_components=1)
