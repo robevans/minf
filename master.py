7ad7faa5324371042dfe9dbeea523627f80bc22c
Robert Evans <rlevans91@gmail.com>
2013-02-12 17:53:44 +0000

    added function to get quaternion segements, other functions can now be passed graph titles when plotting

--- master.py
+++ master.py
@@ -13,7 +13,6 @@ def compareTwoFiles(f1,f2,n_components=3):
 	(ls1,w1) = getLowDimensionalSegments(data1,n_components)
 	(ls2,w2) = getLowDimensionalSegments(data2,n_components)
 	averageWeights = [(a+b)/2.0 for (a,b) in zip(w1,w2)]
-	print averageWeights
 	names = []
 	for i in range(len(ls1)):
 		names.append('1')
@@ -22,24 +21,32 @@ def compareTwoFiles(f1,f2,n_components=3):
 
 	similarityMatrix(ls1+ls2,names,averageWeights)
 
-def similarityMatrix(segments,segmentNames,weights):
+def similarityMatrix(segments,segmentNames,weights,title):
 	distances = []
 	for i in range(len(segments)):
 		distances.append([])
 		for j in range(len(segments)):
 			distances[i].append(dtw.getDTWdist2DweightedSum(segments[i],segments[j],weights))
-	plot.plotSimilarityMatrix(distances,segmentNames)
+	plot.plotSimilarityMatrix(distances,segmentNames,title)
 
-def getLowDimensionalSegments(highDimensionalData,n_components=2):
+def getQuaternionSegmentsByRawData(highDimensionalData,quaternionData):
+	(lowDimensionalData,explainedVariance) = pca.pca(highDimensionalData,n_components=1)
+	(mins,maxs) = segment.segmentationPoints(lowDimensionalData[:,0])
+	segments = pl.split(quaternionData,maxs)[1:-1]
+	return segments
+
+def getLowDimensionalSegments(highDimensionalData,n_components=2,plt=False,title="Latent space segments"):
 	(lowDimensionalData,explainedVariance) = pca.pca(highDimensionalData,n_components)
 	(mins,maxs) = segment.segmentationPoints(lowDimensionalData[:,0])
-	segments = pl.split(lowDimensionalData,maxs)
-	return (segments[1:-1],explainedVariance)
+	segments = pl.split(lowDimensionalData,maxs)[1:-1]
+	if plt:
+		plot.plotGridOf2Ds(segments,title)
+	return (segments,explainedVariance)
 
 def getHighDimensionalSegments(highDimensionalData):
 	(lowDimensionalData,explainedVariance) = pca.pca(highDimensionalData,n_components=1)
 	(mins,maxs) = segment.segmentationPoints(lowDimensionalData[:,0])
-	segments = pl.split(highDimensionalData,maxs)
+	segments = pl.split(highDimensionalData,maxs)[1:-1]
 	return segments
 
 def readCSVfile(input_filename):
