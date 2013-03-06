e2491ffc492a95b7e4b9950e1b99080d4a2a70c4
Robert Evans <rlevans91@gmail.com>
2013-03-06 11:02:48 +0000

    Tweaked code for one Orient.  Old captures were no good because of quantisation bug

--- jointPositions.py
+++ jointPositions.py
@@ -5,25 +5,26 @@ import plot
 import segment
 import pylab as pl
 
-def readJointPositions(input_filename):
+def readJointPositions(input_filename):  #TODO: make this function work with any number of orients
 	with open(input_filename,'r') as f:
 			lines = f.readlines()
 	
-	d4 = []
-	d5 = []
+	#d4 = []
+	#d5 = []
 	d8 = []
 	for l in lines:
 		l= map(float,filter(lambda x: x!='' ,l.translate(None,'(),/n')[:-1].split(' ')))
 		id = l[0]
 		xyz = l[1:]
-		if id == 4: d4.append(xyz)
-		if id == 5: d5.append(xyz)
+		#if id == 4: d4.append(xyz)
+		#if id == 5: d5.append(xyz)
 		if id == 8: d8.append(xyz)
 
-	z=zip(*[d4,d5,d8])
-	data = [[a,b,c,d,e,f,g,h,i] for ([a,b,c],[d,e,f],[g,h,i]) in z]
-
-	return data
+	#z=zip(*[d4,d5,d8])
+	#z=zip(d4,d5,d8) #see if this works also 
+	#data = [[a,b,c,d,e,f,g,h,i] for ([a,b,c],[d,e,f],[g,h,i]) in z]
+	return d8
+	#return data
 
 def plotHighDim(input_filename):
 	data=readJointPositions(input_filename)
