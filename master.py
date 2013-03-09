fbdf7b73fe454390a35ec96aac7d72bc869a9e19
Robert Evans <rlevans91@gmail.com>
2013-03-09 23:24:02 +0000

    Added progress bar for similarity matrix, function to get high and low def segments at once, and functionality to read raw data

--- master.py
+++ master.py
@@ -4,12 +4,18 @@ import pca
 import dtw
 import plot
 import quats
+import numpy
 import segment
 import pylab as pl
+import progressbar
 
-def compareTwoFiles(f1,f2,n_components=3):
-	data1 = readCSVfile(f1)
-	data2 = readCSVfile(f2)
+def compareTwoFiles(f1,f2,n_components=3, raw=False, title="similarityMatrix"):
+	if raw:
+		data1 = readRaw(f1)[26:,4:]
+		data2 = readRaw(f2)[26:,4:]
+	else:
+		data1 = readCSVfile(f1)
+		data2 = readCSVfile(f2)
 	(ls1,w1) = getLowDimensionalSegments(data1,n_components)
 	(ls2,w2) = getLowDimensionalSegments(data2,n_components)
 	averageWeights = [(a+b)/2.0 for (a,b) in zip(w1,w2)]
@@ -19,14 +25,20 @@ def compareTwoFiles(f1,f2,n_components=3):
 	for i in range(len(ls2)):
 		names.append('2')
 
-	similarityMatrix(ls1+ls2,names,averageWeights)
+	similarityMatrix(ls1+ls2,names,averageWeights,title)
 
 def similarityMatrix(segments,segmentNames,weights,title,savePlot=False):
 	distances = []
-	for i in range(len(segments)):
+	bar = progressbar.ProgressBar(maxval=len(segments)**2, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+	bar.start()
+	progressCount = 0
+	for i,k in zip(range(len(segments)),reversed(range(len(segments)))):
 		distances.append([])
 		for j in range(len(segments)):
-			distances[i].append(dtw.getDTWdist2DweightedSum(segments[i],segments[j],weights))
+			distances[i].append(dtw.getDTWdist2DweightedSum(segments[k],segments[j],weights))
+			progressCount+=1
+			bar.update(progressCount)
+	bar.finish()
 	plot.plotSimilarityMatrix(distances,segmentNames,title,savePlot)
 
 def getQuaternionSegmentsByRawData(highDimensionalData,quaternionData):
@@ -49,7 +61,23 @@ def getHighDimensionalSegments(highDimensionalData):
 	segments = pl.split(highDimensionalData,maxs)[1:-1]
 	return segments
 
+def getHighAndLowDimSegments(highDimensionalData, n_components=3):
+	(lowDimensionalData,explainedVariance) = pca.pca(highDimensionalData,n_components)
+	(mins,maxs) = segment.segmentationPoints(lowDimensionalData[:,0])
+	HDsegments = pl.split(highDimensionalData,maxs)[1:-1]
+	LDsegments = pl.split(lowDimensionalData,maxs)[1:-1]
+	return (HDsegments,LDsegments,explainedVariance)
+
 def readCSVfile(input_filename):
 	with open(input_filename,'r') as fin:
 			data = pl.loadtxt(fin,delimiter=",")
 	return data
+
+def readRaw(input_filename):
+	with open(input_filename,'r') as f:
+		lines = f.readlines()
+	data = []
+	for l in lines:
+		l= map(float,filter(lambda x: x!='' ,l.translate(None,',ary()[]/n')[:-1].split(' ')))
+		data.append(l)
+	return numpy.array(data)
